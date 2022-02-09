cb1af010d19edcc77f3035b70e1ebffe1b960a83
Technoboy8871 <72546704+Technoboy8871@users.noreply.github.com>
2022-02-09 19:49:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,15 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBxWXeA8qdGwmUYMgymUC4R0kPaNQepccQ",
+  authDomain: "team-voting-app-b4a13.firebaseapp.com",
+  databaseURL: "https://team-voting-app-b4a13-default-rtdb.firebaseio.com",
+  projectId: "team-voting-app-b4a13",
+  storageBucket: "team-voting-app-b4a13.appspot.com",
+  messagingSenderId: "780266151272",
+  appId: "1:780266151272:web:00ef23fd4f2d646fd6cadd"
+};
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
